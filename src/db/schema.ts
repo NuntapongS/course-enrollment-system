a54bdead0e643bd85f6f482614acf6f178c73130
Nuntapong Siripanyawong <nuntapong@odds.team>
2026-01-23 11:59:29 +0700

    Add user schema definition to the database

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, serial, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  firstName: varchar("first_name", { length: 255 }).notNull(),
+  lastName: varchar("last_name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
